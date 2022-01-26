a113d783ba39f29744971fea3ada63781f165eb7
Mason <masonfilbrandt@gmail.com>
2022-01-25 18:14:00 -0800

    added basic tutorial scene for PolyBrush
    You can find it in the Scenes folder!

---
+++ Assets/ProBuilderDemoSceneCameraController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProBuilderDemoSceneCameraController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float translation = Input.GetAxis("Horizontal") * Time.deltaTime * 10;
+        transform.Translate(translation, 0, 0);
+        if(transform.position.x > 90) {
+            transform.position = new Vector3(90, 1, 0);
+        }
+        if(transform.position.x < 0) {
+            transform.position = new Vector3(0, 1, 0);
+        }
+    }
+}
