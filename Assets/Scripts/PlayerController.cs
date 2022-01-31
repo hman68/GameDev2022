41811a40772c5bd959e515723865d214989eef79
miguelChavezNava <miguel.271009@nv.ccsd.net>
2022-01-30 17:05:18 -0800

    PlayerController
    Adds player controller that moves the player with WASD. Uses horizontal and vertical inputs. Speed can be adjusted from the player inspector.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody playerRB;
+    private float horizontalInput;
+    private float verticalInput;
+    public float speed;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRB = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * speed * verticalInput * Time.deltaTime);
+        transform.Translate(Vector3.right * speed * horizontalInput * Time.deltaTime);
     }
 }
